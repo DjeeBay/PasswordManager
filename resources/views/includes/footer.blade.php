99baa7b569deedc4938739dc71b6fe889a52ab78
JB <jb@bms-engineering.lu>
2024-04-05 13:13:09 +0200

    10.1.2

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="app-footer">
     <div><a href="https://github.com/DjeeBay/PasswordManager" target="_blank" class="text-decoration-none"><i class="cib-github text-dark font-2xl"></i></a></div>
-    <div class="ml-auto">v10.1.1</div>
+    <div class="ml-auto">v10.1.2</div>
 </footer>
